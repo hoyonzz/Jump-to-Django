fb7699511bfcb84dc9d13983337bbbc5496b79a3
hoyong.shin <hoyong6737@gmail.com>
2025-06-23 20:12:37 +0900

    upload_studyfiles

--- 17.앵커기능추가하기/mysite/config/urls.py
+++ 17.앵커기능추가하기/mysite/config/urls.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from django.urls import path, include
-from pybo import views
+from pybo.views import base_views
 
 
 
@@ -8,5 +8,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('pybo/', include('pybo.urls')),
     path('common/', include('common.urls')),
-    path('', views.index, name='index'),
+    path('', base_views.index, name='index'),
 ]
