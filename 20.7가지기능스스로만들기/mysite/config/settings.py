4aebf11c86e66502c926efb67a39c4a0109b6bfa
hoyong.shin <hoyong6737@gmail.com>
2025-08-18 21:39:38 +0900

    upload_studyfiles

--- 20.7가지기능스스로만들기/mysite/config/settings.py
+++ 20.7가지기능스스로만들기/mysite/config/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'pybo',
+    'common',
 ]
 
 MIDDLEWARE = [
@@ -124,3 +125,7 @@ STATICFILES_DIRS = [
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# 로그인 성공 시 자동으로 이동할 URL
+LOGIN_REDIRECT_URL = '/'
+LOGOUT_REDIRECT_URL = '/'
--- 20.7가지기능스스로만들기/mysite/pybo/views.py
+++ 20.7가지기능스스로만들기/mysite/pybo/views.py
@@ -2,13 +2,26 @@ from django.shortcuts import render, get_object_or_404, redirect
 from .models import Question, Answer
 from django.utils import timezone
 from .forms import QuestionForm, AnswerForm
+from django.core.paginator import Paginator
+from django.contrib.auth.decorators import login_required
+from django.contrib import messages
 
 
 
 def index(request):
     # pybo 목록 출력
+
+    # 입력 인자
+    page = request.GET.get('page', '1') # 페이지
+
+    # 조회
     question_list = Question.objects.order_by('-create_date')
-    context = {'question_list':question_list}
+
+    # 페이징 처리
+    paginator = Paginator(question_list, 10) # 페이지당 10개씩 보여 주기
+    page_obj = paginator.get_page(page)
+
+    context = {'question_list': page_obj }
     return render(request, 'pybo/question_list.html', context)
 
 def detail(request, question_id):
@@ -17,6 +30,7 @@ def detail(request, question_id):
     context = {'question':question}
     return render(request, 'pybo/question_detail.html', context)
 
+@login_required(login_url='common:login')
 def answer_create(request, question_id):
     # pybo 답변 등록
     question = get_object_or_404(Question, pk=question_id)
@@ -24,6 +38,7 @@ def answer_create(request, question_id):
         form = AnswerForm(request.POST)
         if form.is_valid():
             answer = form.save(commit=False)
+            answer.author = request.user
             answer.create_date = timezone.now()
             answer.question = question
             answer.save()
@@ -33,12 +48,14 @@ def answer_create(request, question_id):
     context = {'question':question, 'form':form}
     return render(request, 'pybo/question_detail.html', context)
 
+@login_required(login_url='common:login')
 def question_create(request):
     # pybo 질문 등록
     if request.method == "POST":
         form = QuestionForm(request.POST)
         if form.is_valid():
             question = form.save(commit=False)
+            question.author = request.user
             question.create_date = timezone.now()
             question.save()
             return redirect('pybo:index')
@@ -47,3 +64,34 @@ def question_create(request):
     context = {'form':form}
     return render(request, 'pybo/question_form.html', context)
 
+@login_required(login_url='common:login')
+def question_modify(request, question_id):
+    # pybo 질문 수정
+    question = get_object_or_404(Question, pk=question_id)
+    if request.user != question.author:
+        messages.error(request, '수정권한이 없습니다')
+        return redirect('pybo:detail', question_id=question.id)
+    
+    if request.method == "POST":
+        form = QuestionForm(request.POST, instance=question)
+        if form.is_valid():
+            question = form.save(commit=False)
+            question.author = request.user
+            question.modify_date = timezone.now()
+            question.save()
+            return redirect('pybo:detail', question_id=question.id)
+    
+    else:
+        form = QuestionForm(instance=question)
+    context = {'form':form}
+    return render(request, 'pybo/question_form.html', context)
+
+@login_required(login_url='common:login')
+def question_delete(request, question_id):
+    # pybo 질문 삭제
+    question = get_object_or_404(Question, pk=question_id)
+    if request.user != question.author:
+        messages.error(request, '삭제권한이 없습니다')
+        return redirect('pybo:detail', question_id=question.id)
+    question.delete()
+    return redirect('pybo:index')
